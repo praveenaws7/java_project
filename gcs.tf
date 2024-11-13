5ebb9401e6dc0c444a054bc5c123fe784357507b
praveenaws7 <praveenaws7@gmail.com>
2024-11-12 23:27:48 -0500

    reverted back and using the google instance

--- gcs.tf
+++ gcs.tf
@@ -1,28 +1,28 @@
-provider "google" {
-  project = "330224828591"
-  region  = "us-east1"
-}
+# provider "google" {
+#   project = "330224828591"
+#   region  = "us-east1"
+# }
 
-resource "google_storage_bucket" "terraform_state" {
-  name     = "fedramao-gcp"
-  location = "US"
+# resource "google_storage_bucket" "terraform_state" {
+#   name     = "fedramao-gcp"
+#   location = "US"
 
-  versioning {
-    enabled = true
-  }
+#   versioning {
+#     enabled = true
+#   }
 
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [name]
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true
+#     ignore_changes  = [name]
+#   }
+# }
 
-# backend.tf
+# # backend.tf
 
-terraform {
-  backend "gcs" {
-    bucket = "fedramao-gcp"    # The bucket you created
-    prefix = "terraform/state" # Optional: prefix for state files
-    state  = "google-cloud-service.tfstate"
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket = "fedramao-gcp"    # The bucket you created
+#     prefix = "terraform/state" # Optional: prefix for state files
+#     state  = "google-cloud-service.tfstate"
+#   }
+# }
